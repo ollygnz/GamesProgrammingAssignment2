a5566f9d1a0cef6119451ada418d64717e9cf70a
Aidan-Massey <23006140@massey.ac.nz>
2024-05-30 18:39:29 +1200

    Update JumpingGame.java
    Merged ScreenScroller object method with Anna's overlapping detection.

--- src/JumpingGame.java
+++ src/JumpingGame.java
@@ -1,11 +1,13 @@
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Random;
 
 public class JumpingGame extends GameEngine{
     Player player;
     ScreenScroller screenScroller = new ScreenScroller();
+    LinkedList<Thing> queue = new LinkedList<>();
     Image sheet;
     int totalFrames;
     int rows = 3;
@@ -14,8 +16,6 @@ public class JumpingGame extends GameEngine{
     double t;
     double acceleration = -2; //gravity
     int yFloor = 400; //the "floor" that the sprite runs on
-    ArrayList<Obstacle> obstacles;
-    ArrayList<Coin> coins;
     Random rand = new Random();
     AudioClip coinSound;
     AudioClip bgMusic;
@@ -33,13 +33,10 @@ public class JumpingGame extends GameEngine{
         sheet = loadImage("Images/playerSprite.png");
         totalFrames = rows * cols;
         frames = new Image[totalFrames];
-        //sprites = new Image[rows][cols];
         for(int iy = 0; iy < rows; iy++)
         {
             for(int ix = 0; ix < cols; ix++)
             {
-                //Flatten out into 1D array index
-                //System.out.println("x " + ix + " y " + iy);
                 frames[iy * cols + ix] = subImage(sheet, ix * 170, iy * 170, 170, 170);
             }
         }
@@ -47,8 +44,6 @@ public class JumpingGame extends GameEngine{
         loseSound = loadAudio("Audio/lose.wav");
         bgMusic = loadAudio("Audio/bgm.wav");
         t = 0;
-        obstacles = new ArrayList<>();
-        coins = new ArrayList<>();
         startAudioLoop(bgMusic);
     }
     public int getFrame(double d)
@@ -59,53 +54,46 @@ public class JumpingGame extends GameEngine{
 
     public void update(double dt) {
         if(gameState == GameState.Game){
+            ScreenScroller.Scroll(queue);
+            ScreenScroller.ShuffleList(queue);
             t+=dt;
             player.updatePosition(acceleration);
             checkRoof();
             // Spawn obstacles
             if (rand.nextInt(200) < 3) { // adjust the frequency as needed
-                obstacles.add(new Obstacle(800, 100 + rand.nextInt(300), 50, 50, 200));
+                queue.add(new Thing('O',rand.nextInt(10,50),rand.nextInt(10,50),rand.nextInt(400,500),rand.nextInt(0,450)));
             }
 
             // Spawn coins
             if (rand.nextInt(100) < 2) { // adjust the frequency as needed
                 int newCoinY = 40 + rand.nextInt(440);
-                for(Obstacle obstacle : obstacles){
-                    if(newCoinY <= obstacle.yPos - 50 || newCoinY >= obstacle.yPos + 150){
-                        coins.add(new Coin(800, newCoinY, 10, 200));
+                boolean valid = true;
+                for (Thing thingtemp : queue) {
+                    if (thingtemp.getType() == 'O') {
+                        if(newCoinY >= thingtemp.getY() - 50 && newCoinY <= thingtemp.getY() + 150){
+                            valid = false;}
                     }
+                }
+                if(valid){
+                    queue.add(new Thing('C', 25, 25, rand.nextInt(400, 500), rand.nextInt(0, 450)));
                 }
             }
 
-            // Update obstacles
-            for (int i = obstacles.size() - 1; i >= 0; i--) {
-                Obstacle obstacle = obstacles.get(i);
-                obstacle.update(dt);
-                if (obstacle.xPos + obstacle.width < 0) {
-                    obstacles.remove(i);
-                } else if (obstacle.getBounds().intersects(new Rectangle((int) player.hitboxX, (int) player.hitboxY, player.hitboxWidth, player.hitboxHeight))) {
+            for (int i = queue.size() - 1; i >= 0; i--) {
+                Thing object = queue.get(i);
+                if ((object.getBounds().intersects(new Rectangle((int) player.hitboxX, (int) player.hitboxY, player.hitboxWidth, player.hitboxHeight)))&&object.getType()=='O') {
                     // Game over
                     playAudio(loseSound);
                     System.out.println("Game Over! Score: " + score);
                     stopAudioLoop(bgMusic);
                     gameState = GameState.GameOver;
                     break;
-                }
-            }
-
-            // Update coins
-            for (int i = coins.size() - 1; i >= 0; i--) {
-                Coin coin = coins.get(i);
-                coin.update(dt);
-                if (coin.xPos + coin.radius * 2 < 0) {
-                    coins.remove(i);
-                } else if (coin.getBounds().intersects(new Rectangle((int) player.hitboxX, (int) player.hitboxY, player.hitboxWidth, player.hitboxHeight))) {
-                    // Collect coin
+                } else if ((object.getBounds().intersects(new Rectangle((int) player.hitboxX, (int) player.hitboxY, player.hitboxWidth, player.hitboxHeight)))&&object.getType()=='C'){
+                    // Increment Score
                     playAudio(coinSound);
-                    coins.remove(i);
+                    queue.remove(i);
                     score++;
-                    System.out.println("Score: " + score);
-                }
+                    System.out.println("Score: " + score);}
             }
         }
 
@@ -141,20 +129,20 @@ public class JumpingGame extends GameEngine{
             int f = getFrame(1);
             DrawWith1DArray(f);
 
-//        //uncomment this to show the hitbox for testing purposes
-//        changeColor(yellow);
-//        drawRectangle(player.hitboxX, player.hitboxY, player.hitboxWidth, player.hitboxHeight);
             // Draw obstacles
-            changeColor(Color.red);
-            for (Obstacle obstacle : obstacles) {
-                drawRectangle(obstacle.xPos, obstacle.yPos, obstacle.width, obstacle.height);
+            for (Thing temp : queue) {
+                if(temp.Type == 'C') {
+                    changeColor(Color.YELLOW);
+                    drawSolidCircle(temp.getX(), temp.getY(), 12.5);
+                } else{
+                    changeColor(Color.RED);
+                    drawSolidRectangle(temp.getX(), temp.getY(), temp.getW(), temp.getH());
+                }
             }
+            changeColor(Color.white);
+            String scoreString = "Score: " + score;
+            drawText(25, 40, scoreString, "Arial", 25);
 
-            // Draw coins
-            changeColor(Color.yellow);
-            for (Coin coin : coins) {
-                drawCircle(coin.xPos, coin.yPos, coin.radius);
-            }
         } else if(gameState == GameState.GameOver){
             gameOverScreen();
         }
