a779f62ba7ae91292ddac05869b9b8ec706c0b84
Aidan-Massey <23006140@massey.ac.nz>
2024-06-01 13:27:48 +1200

    Update ScreenScroller.java
    Added comments.

--- src/ScreenScroller.java
+++ src/ScreenScroller.java
@@ -4,6 +4,7 @@ import java.util.LinkedList;
 public class ScreenScroller{
 
     public static void Scroll(LinkedList<Thing> LL){
+        //For the length of the list, obtain the item at index i, get the item's x co-ordinate and speed, then subtract the x value by the speed value
         for (int i=0;i<LL.size();i++){
             Thing temp = LL.get(i);
             int temp2 = temp.getX();
@@ -13,6 +14,8 @@ public class ScreenScroller{
     }
 
     public static void ShuffleList(LinkedList<Thing> LL){
+        //For the length of the list, obtain the item at index i, check whether the item's x co-ordinate + width is less than zero 
+        //(i.e not visible on the screen), and remove the item if so
         for (int i=0;i<LL.size();i++){
             Thing temp = LL.get(i);
             if((temp.getX() + temp.getW()) < 0){
